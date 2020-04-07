bc92b89b64762a42b30f2b558e161ac61d00a455
Anirudh Gupta <anirudhgupta109@aosip.dev>
2020-04-07 21:11:09 +0530

    sdm845-common: Transition from system_prop.mk to system.prop
    Signed-off-by: Anirudh Gupta <anirudhgupta109@aosip.dev>
    Change-Id: I6467e54268aa2e07275263d3aedee50730d7467d

--- common.mk
+++ common.mk
@@ -33,9 +33,6 @@ PRODUCT_PACKAGES += \
     OnePlusIconShapeSquircleOverlay \
     OnePlusIconShapeTeardropOverlay
 
-# Properties
--include $(LOCAL_PATH)/system_prop.mk
-
 PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 
 # VNDK
